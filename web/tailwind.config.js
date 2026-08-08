55cc6bb873cd6af6a601b194ee4bf1434de8fb7d
oto <oto102079@gmail.com>
2026-08-08 23:10:00 +0800

    tailwindcss downgrade to 3

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // 根据你的实际技术栈调整
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
